dd04dbe9322fb058d63d5d0b817de4cb89815d4d
Abdullah Bisiriyu <abdullahbisiriyu988@gmail.com>
2025-03-23 14:17:56 +0100

    updated-endpoint

--- src/Constant/index.js
+++ src/Constant/index.js
@@ -1,7 +1,7 @@
 
-// export const BASE_URL = 'https://ideal-cbt-a3ddcdhsbxdcgnbv.westeurope-01.azurewebsites.net';
+export const BASE_URL = 'https://ideal-cbt-a3ddcdhsbxdcgnbv.westeurope-01.azurewebsites.net';
 // export const BASE_URL = 'http://192.168.0.100:7200';
-export const BASE_URL = 'http://localhost:5026';
+// export const BASE_URL = 'http://localhost:5026';
 
 
 export const SCHOOL_NAME= 'iDEAL'
