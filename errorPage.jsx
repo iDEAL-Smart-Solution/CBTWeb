1339ebd0cfded5647b83ec8de4bda95d2674635c
Abdullah Bisiriyu <abdullahbisiriyu988@gmail.com>
2025-06-27 08:57:46 +0100

    update: update

--- errorPage.jsx
+++ errorPage.jsx
@@ -1,13 +1,20 @@
-import logo from "./src/assets/iDEAL-logo.jpg"
+import logo from "./src/assets/iDEAL-logo.jpg";
+
 const ErrorPage = () => {
-     return (
-          <div style={{ width: '70vw', height: '60vh' }}>
-               <h1 className="text-center color-primary">Coming soon!!!</h1>
-               <div style={{ display: 'flex', placeContent: 'center', placeContent: 'center' }}>
-                    <img className="text-center" src={logo} alt="ideal-logo" width={`400em`} />
-               </div>
-          </div>
-     )
-}
+  return (
+    <div className="w-[70vw] min-h-[60vh] flex flex-col items-center justify-center mx-auto">
+      <h1 className="text-center text-blue-600 text-3xl sm:text-4xl md:text-5xl font-bold mb-6 sm:mb-8">
+        Coming soon!!!
+      </h1>
+      <div className="flex justify-center">
+        <img
+          className="max-w-[250px] sm:max-w-[300px] md:max-w-[350px] lg:max-w-[400px] h-auto"  
+          src={logo}
+          alt="ideal-logo"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default ErrorPage;
