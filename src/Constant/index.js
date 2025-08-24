d96019eb763ff0b9469b9e6bd651396ed81acc46
Bigboss298 <abdullahbisiriyu988@gmail.com>
2025-08-24 14:36:55 +0100

    fix: cleaned up dead codes and uploaded the baseurl

--- src/Constant/index.js
+++ src/Constant/index.js
@@ -1,6 +1,5 @@
 
-export const BASE_URL = 'https://ideal-cbt-a3ddcdhsbxdcgnbv.westeurope-01.azurewebsites.net';
-
+export const BASE_URL = 'https://ideal-cbt-api-cwg2bjgxd3hjcea6.westeurope-01.azurewebsites.net/'
 
 
 export const SCHOOL_NAME= 'iDEAL'
