7609a78ccccf0d8c8790c5a96f1b8ab320ce5789
Bigboss298 <abdullahbisiriyu988@gmail.com>
2025-08-24 14:03:43 +0100

    fix

--- src/Pages/auth/index.jsx
+++ src/Pages/auth/index.jsx
@@ -1,17 +1,5 @@
 import LoginForm from "../../Component/auth/loginForm";
 
-// export default function Auth() {
-//      return (
-//           <div className="position-relative min-vh-100 d-flex flex-column">
-//                <LoginForm />
-//           </div>
-//      );
-// }
-
-
-
-
-
 export default function Auth() {
      return <LoginForm />;
    }
